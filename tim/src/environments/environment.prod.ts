377934c8c82e48d670473c04e6f58b128531d619
antoniocarlosti <antoniocarloscosta.ti@gmail.com>
2019-09-08 22:29:14 -0300

    no message

--- tim/src/environments/environment.prod.ts
+++ tim/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://www.esperancareal.com.br/biblia/',
+  url: 'https://www.esperancareal.com.br/biblia/',
   packageFile: './../../../../package.json',
   VERSION: require('../../package.json').version
 };
